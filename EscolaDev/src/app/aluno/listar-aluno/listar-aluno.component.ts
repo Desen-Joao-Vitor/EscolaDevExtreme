7cab34fcde6d9f1835d561ac6f7da12b74b7bc40
MedeirosJoaoV <138505282+MedeirosJoaoV@users.noreply.github.com>
2024-02-23 18:09:20 -0300

    Alterando campo cpf

--- EscolaDev/src/app/aluno/listar-aluno/listar-aluno.component.ts
+++ EscolaDev/src/app/aluno/listar-aluno/listar-aluno.component.ts
@@ -1,3 +1,4 @@
+import { lastValueFrom } from 'rxjs';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AlunosService } from '../alunos-service';
 import { DxDataGridComponent } from 'devextreme-angular';
@@ -16,6 +17,8 @@ export class ListarAlunoComponent implements OnInit {
   estados!: string[];
   data!: any[];
   cepField: any;
+
+  cpfField: any;
   cidadeField: any;
   menssageErro = ' ';
 
@@ -40,34 +43,38 @@ export class ListarAlunoComponent implements OnInit {
   }
 
   registarCep() {
-    // if (this.cepField.length < 9) {
-    //   this.menssageErro = 'Cep deve conter no  minimo 8 caracteres ';
-    //   return this.menssageErro;
-    // } else {
-    //   this.service.ConsultarCep(this.cepField.value).subscribe(
-    //     (data) => {
-    //       console.log(data);
-    //     },
-    //     (error) => {
-    //       console.error('Erro ao consultar CEP:', error);
-    //     }
-    //   );
-    // }
+    if (this.cepField.value.length == 8) {
+      this.service.ConsultarCep(this.cepField.value).subscribe(
+        (data) => {
+          this.cidadeField.component.cellValue(0, 'cidade', data.localidade);
+          this.cidadeField.component.cellValue(0, 'bairro', data.bairro);
+          this.cidadeField.component.cellValue(0, 'rua', data.logradouro);
+          this.cidadeField.component.cellValue(0, 'estado', data.uf);
+        },
+        (error) => {
+          console.error('Erro ao consultar CEP:', error);
+        }
+      );
+    }
   }
 
-  formatarCPF(data: any) {
-    const that = this;
-    // Remove caracteres não numéricos
-    const numerosCpf = that.data.values.arguments.replace(/\D/g, '');
+  formatarCPF() {
+    if (this.cpfField.value.length == 12) {
+      console.log(this.cpfField.value);
+      const that = this;
+      // Remove caracteres não numéricos
+      const numerosCpf = this.cpfField.value.replace(/\D/g, '');
 
-    // Limita o comprimento do CPF para 11 dígitos
-    const cpfFormatado = numerosCpf.slice(0, 11);
+      // Limita o comprimento do CPF para 11 dígitos
+      const cpfFormatado = numerosCpf.slice(0, 11);
 
-    // Formata o CPF com pontos e traço
-    this.data = cpfFormatado.replace(
-      /(\d{3})(\d{3})(\d{3})(\d{2})/,
-      '$1.$2.$3-$4'
-    );
+      // Formata o CPF com pontos e traço
+      this.cpfField = cpfFormatado.replace(
+        /(\d{3})(\d{3})(\d{3})(\d{2})/,
+        '$1.$2.$3-$4'
+      );
+      this.cpfField.componet.component.cellValue(0, 'cpf', this.cpfField);
+    }
   }
   onEditorPreparing(e: any) {
     const that = this;
@@ -82,8 +89,6 @@ export class ListarAlunoComponent implements OnInit {
               defaultFnc(ev);
               this.cepField = ev;
               this.registarCep();
-              console.log(this.cidadeField);
-              this.cidadeField.editorOptions.value = 'Rio Verde';
             };
             e.editorOptions.onValueChanged = fnc.bind(this);
           }
@@ -99,6 +104,16 @@ export class ListarAlunoComponent implements OnInit {
             e.editorOptions.onValueChanged = fnc.bind(this);
           }
           break;
+
+        case 'cpf': {
+          this.cpfField = e;
+          const fnc = (ev: any) => {
+            defaultFnc(ev);
+            this.cpfField = ev;
+            this.formatarCPF();
+          };
+          e.editorOptions.onValueChanged = fnc.bind(this);
+        }
       }
     }
   }
