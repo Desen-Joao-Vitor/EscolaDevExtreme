d31bad9b814d71ef418dd68f4f99afbaffaf3f4b
MedeirosJoaoV <138505282+MedeirosJoaoV@users.noreply.github.com>
2024-04-01 17:07:59 -0300

    Up tela
    Tela de Login dos usuarios

--- EscolaDev/src/app/login/login.component.ts
+++ EscolaDev/src/app/login/login.component.ts
@@ -1,58 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import CustomStore from 'devextreme/data/custom_store';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  usuario = { email: null, senha: null };
-  auxiliar: number = 1;
-  Sussesso!: string;
+export class LoginComponent  {
+  dataUsuariosLogin: any = {};
 
-  Cadastrados = [
-    { usuario: 1, email: 'Teste1@gmail.com', senha: '123456789@' },
-    { usuario: 2, email: 'Teste2@gmail.com', senha: '123456789@' },
-    { usuario: 3, email: 'Teste3@gmail.com', senha: '123456789@' },
-    { usuario: 4, email: 'Teste4@gmail.com', senha: '123456789@' },
-  ];
+  constructor(private serviceLonginUsuario: LoginService) {
+    this.dataUsuariosLogin = serviceLonginUsuario.getDataSource();
+  }
+  Entrar() {
+    // Enviar os dados do formulário para a API
+    this.serviceLonginUsuario.verificarLogin(this.dataUsuariosLogin.value).subscribe(
+      (response ) => {
+        console.log('Resposta da API:', this.dataUsuariosLogin);
 
-  errorMessage: string = '';
-  constructor(private router: Router) {}
-
-  login() {
-    // Validação do email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!this.usuario.email || this.usuario.email == ' ') {
-      this.errorMessage = 'Campo não pode ser vazio!';
-      return;
-    } else if (!emailRegex.test(this.usuario.email)) {
-      this.errorMessage = 'Email inválido!';
-      return;
-    }
-
-    // Validação da senha
-    const passwordRegex = /^(?=.*[@#$%^&+=])(?=.*[a-zA-Z0-9]).{8,}$/;
-    if (!this.usuario.senha || this.usuario.senha == ' ') {
-      this.errorMessage = 'O campo não pode ser vazio';
-    } else if (!passwordRegex.test(this.usuario.senha)) {
-      this.errorMessage =
-        'Senha inválida! Deve conter pelo menos 8 caracteres, incluindo um caractere especial.';
-      return;
-    }
-
-    // Verificar se o usuário existe
-    const user = this.Cadastrados.find(
-      (u) => u.email === this.usuario.email && u.senha === this.usuario.senha
+      },
+      (error :any ) => {
+        console.error(
+          'Erro ao enviar dados para a API:',
+          error,
+          this.dataUsuariosLogin
+        );
+        // Tratar o erro conforme necessário
+      }
     );
-    if (user) {
-      // Usuário encontrado, redirecionar para outra página ou realizar ação desejada
-
-      this.Sussesso = 'Usuário autenticado! Redirecionando...';
-      this.router.navigate(['/Menu']);
-    } else {
-      this.errorMessage = 'Usuário não encontrado!';
-    }
   }
 }
+
+//   // Verificar se o usuário existe
+//   const user = this.Cadastrados.find(
+//     (u) => u.email === this.usuario.email && u.senha === this.usuario.senha
+//   );
+//   if (user) {
+//     // Usuário encontrado, redirecionar para outra página ou realizar ação desejada
+//
+//     this.Sussesso = 'Usuário autenticado! Redirecionando...';
+//     this.router.navigate(['/Menu']);
+//   } else {
+//     this.errorMessage = 'Usuário não encontrado!';
+//   }
+// }
+//  login() {
+//    // Validação do email
+//    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//    if (!this.usuario.email || this.usuario.email == ' ') {
+//      this.errorMessage = 'Campo não pode ser vazio!';
+//      return;
+//    } else if (!emailRegex.test(this.usuario.email)) {
+//      this.errorMessage = 'Email inválido!';
+//      return;
+//    }
+//    // Validação da senha
+//    const passwordRegex = /^(?=.*[@#$%^&+=])(?=.*[a-zA-Z0-9]).{8,}$/;
+//    if (!this.usuario.senha || this.usuario.senha == ' ') {
+//      this.errorMessage = 'O campo não pode ser vazio';
+//    } else if (!passwordRegex.test(this.usuario.senha)) {
+//      this.errorMessage =
+//        'Senha inválida! Deve conter pelo menos 8 caracteres, incluindo um caractere especial.';
+//      return;
+//    }
+//  }
--- EscolaDev/src/app/login/login.module.ts
+++ EscolaDev/src/app/login/login.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutes } from './login.routes';
+import { DxButtonModule, DxFormModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutes, FormsModule],
+  imports: [
+    CommonModule,
+    LoginRoutes,
+    FormsModule,
+    DxFormModule,
+    DxButtonModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
